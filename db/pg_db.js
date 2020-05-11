4639214590a85d222d6fdaca3cd7bd6902f48859
rebbyv <rebsvincent@gmail.com>
2020-05-10 19:59:44 -0500

    Data generation & sedding for postgres

--- db/pg_db.js
+++ db/pg_db.js
@@ -8,6 +8,48 @@ const client = new Client({
   port: 5432,
 });
 
-client.connect(err => {
-  err ? console.error('connection error', err.stack): console.log('connected');
-})
+
+var createProducts = 'CREATE TABLE IF NOT EXISTS products (' +
+  'productNumber integer PRIMARY KEY NOT NULL,' +
+  'productName varchar (200),' +
+  'productDescription text,' +
+  'productCategory varchar (200),' +
+  'storeNumber integer)';
+var copyToProducts = "COPY products FROM '/home/rebbyv/Projects/rpt19-sdc/rebekah-service/sample_data/postgres/pg_data_products.csv' CSV HEADER";
+
+
+var createStyles = 'CREATE TABLE IF NOT EXISTS styles (' +
+  'id integer PRIMARY KEY NOT NULL,' +
+  'style varchar (200),' +
+  'product_id integer,' +
+  'FOREIGN KEY (product_id) REFERENCES products (productNumber))';
+var copyToStyles = "COPY styles FROM '/home/rebbyv/Projects/rpt19-sdc/rebekah-service/sample_data/postgres/pg_data_styles.csv' CSV HEADER";
+
+
+client 
+  .connect()
+  .then(() => {
+    console.log('connected');
+    client.query(createProducts)
+    .then(() => {
+      client.query(copyToProducts)
+        .then((results) => {
+          console.log(`${results.rowCount} rows inserted.`)
+        })
+        . catch(err => console.log('products copy error' ,err))
+      })
+      .catch(err => console.log('products table creation error', err))
+    })
+  .then(() => {
+    client.query(createStyles)
+    .then(() => {
+      client.query(copyToStyles)
+      .then((results) => {
+        console.log(`${results.rowCount} rows inserted.`)
+        client.end();
+      })
+      .catch(err => console.log('styles copy error' ,err))
+    })
+    .catch(err => console.log('styles table creation error', err))
+  })
+  .catch(err => console.log('connection error', err))
